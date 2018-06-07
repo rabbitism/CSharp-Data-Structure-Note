b92e3228138bb3d19eb2b89aaca439ba4f685b54
rabbitism <popmessiah@126.com>
2018-06-07 15:07:21 +0800

    Set up Generic project, Add timer class.

---
+++ Generic/Generic/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Generic
+{
+    public class Timer
+    {
+        TimeSpan startingTime;
+        TimeSpan duration;
+
+        public Timer(){
+            startingTime = new TimeSpan(0);
+            duration = new TimeSpan(0);
+        }
+
+        public void Stop(){
+            duration = Process.GetCurrentProcess().Threads[0].UserProcessorTime.Subtract(startingTime);
+        }
+
+        public void startTime(){
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            startingTime = Process.GetCurrentProcess().Threads[0].UserProcessorTime;
+        }
+
+        public TimeSpan Result(){
+            return duration;
+        }
+
+        public string GetDuration(){
+            return duration.TotalMilliseconds.ToString();
+        }
+    }
+}
